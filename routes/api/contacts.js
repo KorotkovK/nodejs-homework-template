7b5be249c618901af3fc0b4a39d9aaa79f147a8e
KorotkovK <korotkov.kostya123@gmail.com>
2024-01-10 13:53:27 +0200

    Homework02

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,35 @@
-const express = require('express')
+// routes/api/contacts.js
+const express = require('express');
+const router = express.Router();
+const contactsModel = require('../../models/contacts');
 
-const router = express.Router()
+router.get('/', async (req, res) => {
+  const contacts = await contactsModel.listContacts();
+  res.json(contacts);
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:id', async (req, res) => {
+  const contact = await contactsModel.getContactById(req.params.id);
+  if (contact) {
+    res.json(contact);
+  } else {
+    res.status(404).json({ message: 'Not found' });
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', async (req, res) => {
+  const result = await contactsModel.addContact(req.body);
+  res.status(result.status).json(result);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:id', async (req, res) => {
+  const result = await contactsModel.removeContact(req.params.id);
+  res.status(result.status).json(result);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put('/:id', async (req, res) => {
+  const result = await contactsModel.updateContact(req.params.id, req.body);
+  res.status(result.status).json(result);
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
